bd1e71528c8c605fcb218e80a952f82514b30961
Zhicheng Ji <zhichengji@gmail.com>
2016-01-23 21:28:50 -0500

    edit namespace

--- R/SCRATui.R
+++ R/SCRATui.R
@@ -7,7 +7,7 @@
 #' However, it is highly recommended that the user interface be launched locally for faster running speed.
 #' 
 #' @export
-#' @import shiny GenomicAlignments ggplot2 reshape2 pheatmap scatterD3 SCRATdata
+#' @import shiny GenomicAlignments ggplot2 reshape2 pheatmap scatterD3 
 #' @author Zhicheng Ji, Weiqiang Zhou, Hongkai Ji <zji4@@zji4.edu>
 #' @examples
 #' \dontrun{
