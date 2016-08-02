a3ecf38c1cda106b35d78eb02fe58c494a4fedc0
Weiqiang Zhou <kenzhou86@gmail.com>
2016-08-02 10:17:01 -0500

    Update server.R

--- inst/shiny/server.R
+++ inst/shiny/server.R
@@ -5,7 +5,6 @@
 ##   Author:Zhicheng Ji, Weiqiang Zhou, Hongkai Ji  ##
 ##       Maintainer:Zhicheng Ji (zji4@jhu.edu)      ##
 ######################################################
-##testing
 library(shiny)
 library(d3heatmap)
 library(GenomicAlignments)
